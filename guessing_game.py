9f190eb9f5df24bd91817ddeae008e4c57466d86
Branwyn Gwenyn Tylwyth <35962897+mormorhaxa@users.noreply.github.com>
2020-05-08 09:30:09 -0700

    Updated for PEP8
    Ran a style check and corrected the errors.

--- guessing_game.py
+++ guessing_game.py
@@ -2,6 +2,7 @@ import random
 
 high_score = 1000
 
+
 def display_welcome():
     print("""=================================
 Welcome, Friends, to the Game
@@ -12,8 +13,10 @@ Welcome, Friends, to the Game
     if high_score < 1000:
         print("The record thus far is {} guesses.".format(high_score))
 
+
 def create_target_number(low, high):
     return random.randint(low, high)
+
 
 def offer_retry():
     keep_playing = input("Would you like to play again?")
@@ -28,6 +31,7 @@ def offer_retry():
         print("I can only understand 'Y' or 'N'.")
         offer_retry()
 
+
 def start_game():
     global high_score
         
@@ -37,7 +41,7 @@ def start_game():
     total_guesses = 0
     target_number = create_target_number(low_number, high_number)
 
-    display_welcome()            
+    display_welcome()
 
     while True:
         guess = input("Guess a number from 1 - 10:  ")
@@ -45,25 +49,33 @@ def start_game():
         try:
             guess = int(guess)
         except ValueError:
-            print("\n'{}' is not valid. You must enter only whole numbers from {} to {}.\n\nTry again!".format(guess, low_number, high_number))
+            print(
+                f"\n'{guess}' is not valid. "
+                f"You must guess whole numbers from {low_number} to "
+                f"{high_number}.\n\nTry again!"
+                )
             continue
 
         total_guesses += 1
         
         if guess < low_number or guess > high_number:
-            print("\nYour guess must be a number between {} and {}. Try again!".format(low_number, high_number))
+            print(
+                f"\nYour guess must be a number between {low_number} "
+                f"and {high_number}. Try again!")
             continue
         elif guess > target_number:
-            print("\nThe number is lower than {}. Try again!".format(guess))
+            print(f"\nThe number is lower than {guess}. Try again!")
             continue
         elif guess < target_number:
-            print("\nThe number is higher than {}. Try again!".format(guess))
+            print(f"\nThe number is higher than {guess}. Try again!")
             continue
         elif guess == target_number:
-            print("\nYou guessed the number in {} tries!\nYou win!".format(total_guesses))
+            print(
+                f"\nYou guessed the number in {total_guesses} tries!\nYou win!"
+                )
             break
     if total_guesses < high_score:
-        print("You've set a new record of {} guesses!".format(total_guesses))
+        print(f"You've set a new record of {total_guesses} guesses!")
         high_score = total_guesses
         
     offer_retry()
